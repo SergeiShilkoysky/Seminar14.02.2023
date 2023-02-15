005ebf128656a8ff0dd809c23954968853e93b6e
Sergei Sh <sergshrb@mail.ru>
2023-02-15 11:35:18 +0300

    Вывод наиб.цифры случайного числа в [10, 99]

--- lesson1/Program.cs
+++ lesson1/Program.cs
@@ -1,17 +1,16 @@
 ﻿//Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа
-
-//78 -> 8
+//78 -> 8  / 12 ->2   / 85-> 8
 
 Console.Clear();
 
 int num = new Random().Next(10,100);
 Console.WriteLine($"Число: {num}");
 
-int firstValue = num / 10;
-int secondValue = num % 10;
+int firstValue = num / 10; // первая цифра - результат целочисленного деления
+int secondValue = num % 10; // 
 
 int max = firstValue;
-if(secondValue > max){
+if(secondValue > max) {
     max = secondValue;
 }
 
