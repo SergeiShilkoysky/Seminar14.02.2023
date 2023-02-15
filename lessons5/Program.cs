9d2132af1dd1f54c4c3591a79f6c4fc09193c305
Sergei Sh <sergshrb@mail.ru>
2023-02-15 17:36:21 +0300

    2 варианта проверки является ли число квадратом другого

--- lessons5/Program.cs
+++ lessons5/Program.cs
@@ -1,11 +1,11 @@
 ﻿// Напишите программу, которая приимает на вход два числа и проверяет, является ли одно число квадратом другого
 // 5, 25 -> да / -4, 16 -> да / 25, 5 -> да / 8, 9 -> нет
 
-/* Console.Write("Введите число1: ");
+/*Console.Write("Введите число1: ");
 Double num1 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите число2: ");
 Double num2 = Convert.ToDouble(Console.ReadLine());
-if (Math.Sqrt(Math.Abs(num1)) == Math.Abs(num2) || Math.Sqrt(Math.Abs(num2)) == Math.Abs(num1)) // Math.Abs т.к.учесть из отриц.корень
+if (Math.Sqrt(Math.Abs(num1)) == num2 || Math.Sqrt(Math.Abs(num2)) == num1) // Math.Abs т.к.учесть из отриц.корень
 {
     Console.WriteLine($"{num1}, {num2} одно из чисел является квадратом другого");    
 }
@@ -13,15 +13,16 @@ else
 {
     Console.WriteLine($"{num1}, {num2} -> не является");
 }
-*/
+/// в первом варианте осталась проблема с -числами
+*/ 
 
 Console.Write("Введите число1: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
+double num1 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите число2: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-if (Math.Pow(num1, 2) == num2 || Math.Pow(num2) == num1)
+double num2 = Convert.ToDouble(Console.ReadLine());
+if (Math.Pow(num1, 2) == num2 || Math.Pow(num2, 2) == num1)
 {
-    Console.WriteLine($"{num1}, {num2} одно из чисел является квадратом другого");    
+    Console.WriteLine($"{num1}, {num2} одно из чисел является квадратом другого");
 }
 else
 {
