315a4b5246aca967e03b8c2446bb09f6841cfaa0
Sergei Sh <sergshrb@mail.ru>
2023-02-17 09:45:03 +0300

    ДЗ №10 показать 2ю цифру 3х значного числа

---
+++ Homework1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным. 6 -> да  /  7 -> да  /  1 -> нет
+
+Console.Clear();
+Console.Write("Введите номер дня недели: ");
+int numWeek = Convert.ToInt32(Console.ReadLine());
+
+if (numWeek == 6 || numWeek == 7)
+{
+    Console.WriteLine($"этот день ВЫХОДНОЙ !!!");
+}
+else if (numWeek ==1 || numWeek == 2 || numWeek == 3 || numWeek == 4 || numWeek == 5)
+{
+    Console.WriteLine(":( будни...");
+}
+else
+{
+    Console.WriteLine("Введите правильный номер");
+}
